a2571ae5448aa389c8d321ae741aec59717c90c3
Victoria Earl <kitsuta@gmail.com>
2017-04-07 10:24:48 -0400

    Remove all references to free food for staff Rips out all the emails and templates about free food for staff and guests. Part of the fix for https://github.com/magfest/magclassic/issues/64.

--- magclassic/automated_emails.py
+++ magclassic/automated_emails.py
@@ -1,18 +1,5 @@
 from magclassic import *
 
-
-AutomatedEmail(Attendee, '{EVENT_NAME} food for guests', 'guest_food_restrictions.txt',
-           lambda a: a.badge_type == c.GUEST_BADGE,
-           ident='maglabs_guest_food_restrictions',
-           sender="MAGFest Staff Suite <chefs@magfest.org>")
-AutomatedEmail(Attendee, '{EVENT_NAME} hospitality suite information', 'guest_food_info.txt',
-           lambda a: a.badge_type == c.GUEST_BADGE,
-           ident='maglabs_guest_food_info',
-           sender="MAGFest Staff Suite <chefs@magfest.org>")
-AutomatedEmail(Attendee, '{EVENT_NAME} Volunteer Food', 'volunteer_food_info.txt',
-           lambda a: a.staffing and days_before(7, c.FINAL_EMAIL_DEADLINE),
-           ident='maglabs_volunteer_food_info',
-           sender="MAGFest Staff Suite <chefs@magfest.org>")
 AutomatedEmail(Attendee, '{EVENT_NAME} Check-in Barcode', 'checkin_barcode.html',
                lambda a: a.badge_status == c.COMPLETED_STATUS and days_before(7, c.FINAL_EMAIL_DEADLINE),
                ident='maglabs_checkin_barcode')
