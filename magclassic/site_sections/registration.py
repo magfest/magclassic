3dabbbd2fd0ffe2d1f03a54b7ab6a42fbb300851
Rob Ruana <rob@robruana.com>
2017-04-27 13:46:51 -0400

    Adds ajax badge animation to magclassic per Dare & Joe's request
    See https://github.com/magfest/ubersystem/pull/2451 for original pull request.

---
+++ magclassic/site_sections/registration.py
@@ -0,0 +1,13 @@
+import random
+from uber.common import *
+
+
+@all_renderable(c.PEOPLE, c.REG_AT_CON)
+class Root:
+    @unrestricted
+    def badges_animation(self):
+        return {'sold': c.BADGES_SOLD}
+
+    @ajax_gettable
+    def badge_stats(self):
+        return {'sold': random.randint(1, 100)}
