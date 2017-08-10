52b2d47de57e9766a19a724a2f0b99bb6737c640
Victoria Earl <kitsuta@gmail.com>
2017-08-10 16:54:05 -0400

    Add updated room assignment email (#89)
    * Add updated room assignment template
    Fixes https://github.com/magfest/magclassic/issues/88. I think.
    
    * Fix spacing
    
    Copy-pasting the snippet removed some double-spaces, but they're important in text templates. Added them back manually.

--- magclassic/automated_emails.py
+++ magclassic/automated_emails.py
@@ -3,6 +3,11 @@ from magclassic import *
 AutomatedEmail(Attendee, '{EVENT_NAME} Check-in Barcode', 'checkin_barcode.html',
                lambda a: a.badge_status == c.COMPLETED_STATUS and days_before(7, c.FINAL_EMAIL_DEADLINE),
                ident='maglabs_checkin_barcode')
+
 AutomatedEmail(Attendee, '{EVENT_NAME} FAQ', 'prefest_faq.html',
                lambda a: a.badge_status == c.COMPLETED_STATUS and days_before(7, c.FINAL_EMAIL_DEADLINE),
                ident='maglabs_prefest_faq')
+
+AutomatedEmail(Room, '{EVENT_NAME} Hotel Room Assignment Update', 'room_assignment_update.txt', lambda r: r.locked_in,
+               sender=c.ROOM_EMAIL_SENDER,
+               ident='hotel_room_assignment')
