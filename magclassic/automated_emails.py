66417770d72239964632bcd654bc9878828a3ef0
Rob Ruana <rob@robruana.com>
2018-02-04 17:47:14 -0500

    Incorporates hotel plugin into uber plugin

--- magclassic/automated_emails.py
+++ magclassic/automated_emails.py
@@ -1,5 +1,5 @@
 from magclassic import *
-from hotel import *
+from uber.models import Room
 
 AutomatedEmail(Attendee, '{EVENT_NAME} Check-in Barcode', 'checkin_barcode.html',
                lambda a: a.badge_status == c.COMPLETED_STATUS and days_before(7, c.FINAL_EMAIL_DEADLINE),
@@ -9,6 +9,6 @@ AutomatedEmail(Attendee, '{EVENT_NAME} FAQ', 'prefest_faq.html',
                lambda a: a.badge_status == c.COMPLETED_STATUS and days_before(7, c.FINAL_EMAIL_DEADLINE),
                ident='maglabs_prefest_faq')
 
-AutomatedEmail(Room, '{EVENT_NAME} Hotel Room Assignment Update', 'room_assignment_update.txt', lambda r: r.locked_in,
+AutomatedEmail(Room, '{EVENT_NAME} Hotel Room Assignment Update', 'hotel/room_assignment_update.txt', lambda r: r.locked_in,
                sender=c.ROOM_EMAIL_SENDER,
                ident='hotel_room_assignment_update')
