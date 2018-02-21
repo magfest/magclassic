6b8063aded8101a824b2897f41c5a68e0d020732
Rob Ruana <rob@robruana.com>
2018-02-21 11:20:58 -0500

    Runs flake8 against entire project

--- magclassic/automated_emails.py
+++ magclassic/automated_emails.py
@@ -1,14 +1,20 @@
-from magclassic import *
-from uber.models import Room
+from uber.automated_emails_server import AutomatedEmail
+from uber.config import c
+from uber.models import Attendee, Room
+from uber.utils import days_before
 
-AutomatedEmail(Attendee, '{EVENT_NAME} Check-in Barcode', 'checkin_barcode.html',
+
+AutomatedEmail(Attendee, '{EVENT_NAME} Check-in Barcode',
+               'checkin_barcode.html',
                lambda a: a.badge_status == c.COMPLETED_STATUS and days_before(7, c.FINAL_EMAIL_DEADLINE),
                ident='maglabs_checkin_barcode')
 
-AutomatedEmail(Attendee, '{EVENT_NAME} FAQ', 'prefest_faq.html',
+AutomatedEmail(Attendee, '{EVENT_NAME} FAQ',
+               'prefest_faq.html',
                lambda a: a.badge_status == c.COMPLETED_STATUS and days_before(7, c.FINAL_EMAIL_DEADLINE),
                ident='maglabs_prefest_faq')
 
-AutomatedEmail(Room, '{EVENT_NAME} Hotel Room Assignment Update', 'hotel/room_assignment_update.txt', lambda r: r.locked_in,
+AutomatedEmail(Room, '{EVENT_NAME} Hotel Room Assignment Update',
+               'hotel/room_assignment_update.txt', lambda r: r.locked_in,
                sender=c.ROOM_EMAIL_SENDER,
                ident='hotel_room_assignment_update')
